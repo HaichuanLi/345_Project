1d0ca678d1cf936e533e2dcbcc836066db0fac0d
Patrick Ng <1.emily.anne@gmail.com>
2026-03-23 20:42:33 -0400

    created all frontend components and pages according to mockups

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,12 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true   // fixed: "is" prefix required in Kotlin DSL
+        }
+    }
 }
 
 dependencies {
@@ -44,7 +50,12 @@ dependencies {
     implementation("androidx.activity:activity:1.9.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
+    // Unit tests
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("androidx.test:core:1.5.0")
+
+    // Instrumented / Espresso tests
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
